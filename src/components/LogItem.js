4c319c7b40f66cf88935f1e11ed81f6378a1be30
Bethanie Innes <doloresdadd@gmail.com>
2020-10-12 14:44:52 +1100

    Display Logs

---
+++ src/components/LogItem.js
@@ -0,0 +1,37 @@
+import React from "react";
+import Moment from "react-moment";
+import Button from "react-bootstrap/Button";
+import Badge from "react-bootstrap/Badge";
+
+const LogItem = ({ log: { priority, text, user, created, _id } }) => {
+  const setVariant = () => {
+    if (priority === "high") {
+      return "danger";
+    } else if (priority === "moderate") {
+      return "warning";
+    } else {
+      return "success";
+    }
+  };
+  return (
+    <tr>
+      <td>
+        <Badge variant={setVariant()} className="p-2">
+          {priority.charAt(0).toUpperCase() + priority.slice(1)}
+        </Badge>
+      </td>
+      <td>{text}</td>
+      <td>{user}</td>
+      <td>
+        <Moment format="Do MMMM YYYY, h:mm:ss a">{new Date(created)}</Moment>
+      </td>
+      <td>
+        <Button variant="danger" size="sm">
+          x
+        </Button>
+      </td>
+    </tr>
+  );
+};
+
+export default LogItem;
